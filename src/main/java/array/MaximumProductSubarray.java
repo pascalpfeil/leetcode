e8fe990340388dd11ad9998e0b02c8e960063713
Pascal Pfeil <pfeilpascal@outlook.de>
2019-09-21 17:02:32 +0200

    MaximumProductSubarray

---
+++ src/main/java/array/MaximumProductSubarray.java
@@ -0,0 +1,41 @@
+package array;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class MaximumProductSubarray {
+    public static int run(int[] nums) {
+        return new MaximumProductSubarray().maxProduct(nums);
+    }
+
+    /**
+     * Default version
+     * <p>
+     * Time: O(n) <br>
+     * Space: O(1)
+     */
+    private int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int maxProduct = nums[0];
+        int currentMin = nums[0];
+        int currentMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int current1 = nums[i];
+            int current2 = current1 * currentMax;
+            int current3 = current1 * currentMin;
+
+
+            currentMin = min(current1, min(current2, current3));
+            currentMax = max(current1, max(current2, current3));
+
+            if (currentMax > maxProduct)
+                maxProduct = currentMax;
+        }
+
+        return maxProduct;
+    }
+}
